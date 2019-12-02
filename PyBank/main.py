a858be79d9da371aede99c976d62a5f423c5c843
Young You <yhyou625@gmail.com>
2019-12-01 16:30:52 -0800

    my first commit

---
+++ PyBank/main.py
@@ -0,0 +1,18 @@
+import os
+import csv
+election_csv = os.path.join ('..','Resourcese','election_data.csv')
+#The percentage of votes each candidate won
+def print_percentage(election_data)
+    #assain valuse
+    Voter ID = str (election_data [0])
+    County  = str (election_data [1])
+    Candidate = str (election_data [2])    
+
+
+The total number of votes cast
+A complete list of candidates who received votes
+
+
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#he winner of the election based on popular vote.
---
+++ PyPoll/main.py
@@ -0,0 +1,34 @@
+import os
+import csv
+budget_csv= os.path.join('..','resources', 'budget_data.csv')
+def print_avarage(budget_data)
+    date =str(budget_data[0])
+    Profit/Losses = int(budget_data[1])
+total_months = data
+def funcname(parameter_list):
+    pass
+total_Profit/Losses =  Profit/Losses
+average([Profit/Losses]=
+
+#def total += (month):
+    #print (total)
+# average of the changes in "Profit/Losses" over the entire period
+#def average (Profit/Losses)
+    #lenth =len (numbers)
+    #total =
+    #for number in numbers:
+       # total +=number
+        #total/length
+print (total_months)
+print (average([Profit/Losses])
+if  Increase in Profits = highest
+Print (Greatest Increase in Profits: date, amount
+if  Decrease in Profits = loweast
+print (Greatest Decrease in Profits: 
+
+#open and read csv
+with open(budget_csv, 'r') as csvfile:
+
+   # csvreader = csv.reader(csvfile, delimiter=',')
+#prompt the th user for what liketo search for 
+Profit/Losses to check =input(Greatest Increase in Profits: date, amount)
