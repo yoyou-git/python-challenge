0aa1b157eb1df7eb890e05b6fe0920f429399350
Young You <yhyou625@gmail.com>
2020-04-08 16:45:05 -0700

    submit pybank too late

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,48 +5,52 @@ budget_csv = os.path.join('..','resources','budget_data.csv')
 
 csvreader = csv.reader(budget_csv , delimiter =',')
 
+#read in the CSV file
+with open('budget_data.csv','r') as csvfile:
+    csvreader =csv.reader(csvfile, delimiter =',')   
+    header_row=next(csvreader)
+    prev_row=next(csvreader)
+    total_month = 1
+    total_profit = int(prev_row[1])
+    total_changes =[]
+    greatest_increase=["",-100000000]
+    greatest_decrease=["",100000000]
+    for eachline in csvreader:
+        # print (eachline) 
+        total_month=total_month+1
+        total_profit=total_profit+int(eachline[1])
+        change=int(eachline[1])-int(prev_row[1])
+        total_changes.append(change)
+        prev_row=eachline
+        # if  Increase in Profits = highest
+        if change > greatest_increase[1]:
+            greatest_increase[0]=eachline[0]
+            greatest_increase[1]=change
+        # if  Decrease in Profits = loweast
+        if change < greatest_decrease[1]:
+            greatest_decrease[0]=eachline[0]
+            greatest_decrease[1]=change
 
-# def Print_mean(budget_data):
+        # Print (Greatest Increase in Profits: date, amount)
+        # if  Decrease in Profits = loweast
+        # print (Greatest Decrease in Profits: date, amount)
+# print (total_month)
+# print(total_profit)
+# print(sum(total_changes)/len(total_changes))
+# # Print (Greatest Increase in Profits: date, amount)
+# print (f"{greatest_increase[0]},${greatest_increase[1]}")
 
-#     date = str (budget_data[0])
-#     profit/Losses =int(budget_data[1])
-
-#    #fine total number of data and amount of profit/Losses
-#     total months = len(df.axes[0])
-#     total amount = profit/Losses
-
-#     average_Profit/Losses =(total amount/total months)
-
-# #print out  
-# print (total months)
-# print (total amount)
-
-# #read in the CSV file
-# with open(budget_csv,'r') as csvfile:
-#  csvreader =csv.reader(csvfile, delimiter =',')   
-# Budget,['profit/Losses'].sort_values(ascending=False) 
-# for eachline in sort:
-#     Print eachline 
-# if  Increase in Profits = highest
-# Print (Greatest Increase in Profits: date, amount)
-# if  Decrease in Profits = loweast
-# print (Greatest Decrease in Profits: date, amount)
-
-
-#def average (Profit/Losses)
-    #lenth =len (date)
-    #total =
-    #for number in Profit/Losses :
-       # total +=number
-      #return  total/length
-#print (total_date)
-
-#if  Increase in Profits = highest
-#Print (Greatest Increase in Profits: date, amount
-#if  Decrease in Profits = loweast
-#print (Greatest Decrease in Profits: date, 
-
-#prompt the th user for what liketo search for 
-#Profit/Losses to check =input(Greatest Increase in Profits: date, amount)
-
-
+# # print (Greatest Decrease in Profits: date, amount)
+# print (f"{greatest_decrease[0]},${greatest_decrease[1]}")
+output=f"""output
+==========================================
+Total month: {total_month}
+Total profit: ${total_profit}
+Average change: ${sum(total_changes)/len(total_changes)}
+Greatest increase: {greatest_increase[0]}, ${greatest_increase[1]}
+Greatest_decrease: {greatest_decrease[0]}, ${greatest_decrease[1]}
+"""
+print (output)
+with open("output.txt","w") as text_file:
+    text_file.write (output)
+   
