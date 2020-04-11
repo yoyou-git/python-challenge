9e8e35e14adf4e3adac699454195ba1dc36741ff
Young You <yhyou625@gmail.com>
2020-04-10 20:28:02 -0700

    submission of pybank and pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,24 +1,38 @@
-inport os
+
 import csv
 with open('election_data.csv','r') as csv_file:
-    csv_reader =csv.rader(csv_file)
-    print line in csv_reader:
-        print (line)
-election_csv = os.path.join ('..','Resourcese','election_data.csv')
+    csv_reader =csv.reader(csv_file)
+    header_row=next(csv_reader)
+    total_votes=0
+    candidates ={}
+    for line in csv_reader:
+        total_votes+=1
+        try:
+            candidates[line[2]]+=1
+        except:
+            candidates[line[2]]=1 
 
-#The percentage of votes each candidate wodef print_percentage(election_data)
-    #assain valuse
-    Voter ID = str (election_data [0])
-    County  = str (election_data [1])
-    Candidate = str (election_data [2])    
+def percent(vote):
+    return round(vote/total_votes*100,2)
 
-total_Voter
+my_list=[]
+winner=["",0]
+for key,value in candidates.items():
+    my_string=f"{key}: {percent(value)}% ({value})" 
+    my_list.append(my_string)
+    if value>winner[1]:
+        winner[0]=key
+        winner[1]=value
 
-##The total number of votes cast
-total_Voter= len(df.axes[0])
 
-#A complete list of candidates who received votes
-
-#The percentage of votes each candidate won
-#The total number of votes each candidate won
-#he winner of the election based on popular vote.
+n="\n"
+output=f"""
+Election Results
+=======================
+total vote: {total_votes}
+=======================
+{n.join(my_list)}
+========================
+winner: {winner[0]}
+"""
+print(output)
